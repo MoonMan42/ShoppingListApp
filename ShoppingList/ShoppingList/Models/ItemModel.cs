fd27d38b75dd7bf9d347d692f25640898a7364e6
MoonMan42 <miller42matt@gmail.com>
2019-06-07 17:03:36 -0400

    Add project files.

---
+++ ShoppingList/ShoppingList/Models/ItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ShoppingList.Models
+{
+    public class ItemModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
